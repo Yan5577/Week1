c1f3faf913e75296b6766ea8e2b8de53930b3193
Yan5577 <zuoyan775@gmai.com>
2022-09-30 19:22:48 +0100

    update

--- variables.js
+++ variables.js
@@ -1,4 +1,10 @@
-let name = "yan";
- age = 37;
- favouriteColour = "blue";
-console.log(`her name is ${name}, she is ${age}, her favcolour is ${favouriteColour}.`);
+// console.log("All Around the World".charAt(7).toUpperCase())
+
+let favDrink = "coffee";
+console.log(favDrink);
+
+
+//let name = "yan";
+ //age = 37;
+ //favouriteColour = "blue";
+//console.log(`her name is ${name}, she is ${age}, her favcolour is ${favouriteColour}.`);
